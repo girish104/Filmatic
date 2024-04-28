d7bd6fc8357d0711300b996eb773d9cdd42de7ac
Girish Pareek <121708026+girish104@users.noreply.github.com>
2024-04-28 11:27:07 +0530

    Update index.blade.php

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -7,15 +7,14 @@
             <div class="grid grid-cols-1 m-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
                 <!-- Example Card -->
                 @foreach ($movies as $movie)
-                    <div class="bg-gray-200 mb-10 rounded-lg overflow-hidden shadow-md ml-auto hover:shadow-lg">
+                    <div class="bg-gray-200 mb-10 rounded-lg overflow-hidden shadow-md">
                         <div class="poster-container" style="height: 300px;">
                             <img src="https://image.tmdb.org/t/p/w300{{ $movie->poster_path }}" alt="{{ $movie->title }}"
-                                class="w-full h-full object-cover transition-transform duration-300 transform hover:scale-105">
+                                 class="w-full h-full object-cover transition-transform duration-300 transform hover:scale-105">
                         </div>
 
-                        <div class="p-4">
-                            <a href="{{ route('movies.show', $movie->id) }}"
-                                class="text-lg font-bold mb-2">{{ $movie->title }}</a>
+                        <div class="p-4 flex flex-col justify-center items-center">
+                            <a href="{{ route('movies.show', $movie->id) }}" class="text-lg font-bold mb-2">{{ $movie->title }}</a>
                             <p class="text-gray-600">Rating : {{ $movie->vote_average }}</p>
                         </div>
                     </div>
